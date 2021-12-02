a57c37395ac446fcd8ca2ef890f7cf7a6261b2ac
Valeria Aguilar <valeriaaguilar@live.com>
2021-12-02 11:16:06 -0800

    landing page is created

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,16 @@
+
+
+export default function Landing() {
+  
+  return (
+    <div>
+      <h1>Welcome to Healthy Pets!</h1>
+      <hr />
+      <h2> I am a ...</h2>
+      <> ICON </> <> ICON </>
+      <button> Veterinarian </button>
+      <button> Pet Owner </button>
+     
+    </div>
+  );
+}

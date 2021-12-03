9bccffb248eefb7206e28c7922864897caff1c3e
Yi Liu <elenaliu0415@gmail.com>
2021-12-02 19:47:06 -0800

    logoNav and logoHome navbars created

---
+++ client/src/components/LogoNav.js
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+
+export default function Logo() {
+  
+  return (
+    <nav className="navbar">
+      <div> 
+      LOGO
+      </div>
+    </nav>
+  );
+}

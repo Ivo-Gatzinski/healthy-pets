e0552f7a562caba43eeac3fe9fced4d6ecb6aec5
Valeria Aguilar <valeriaaguilar@live.com>
2021-12-02 20:20:48 -0800

    app.js complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import { ApolloProvider } from "@apollo/client";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import PrivateRoute from "./components/PrivateRoute";
 import Landing from "./pages/Landing";
-import Login from "./pages/OwnerLogin";
 import ProtectedPageExample from "./pages/ProtectedPageExample";
-import SignUp from "./pages/OwnerSignUp";
+import AddNotes from "./pages/AddNotes";
+import AddPets from  "./pages/AddPets";
+import VetPets from "./pages/VetPets"
+import VetLogin from "./pages/VetLogin";
+import VetSignUp from "./pages/VetSignUp";
+import VetNotes from "./pages/VetNotes"
+import OwnerHome from "./pages/OwnerHome";
+import OwnerLogin from "./pages/OwnerLogin";
+import OwnerSignUp from "./pages/OwnerSignUp";
+import OwnerNotes from "./pages/OwnerNotes";
 import { client } from "./util/apolloClient";
 import { AuthProvider } from "./util/auth";
 
@@ -14,16 +21,40 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <AuthProvider>
-          <Navbar />
+       
           <Switch>
             <Route exact path="/">
               <Landing />
             </Route>
             <Route path="/login">
-              <Login />
+              <VetLogin />
+            </Route>
+            <Route path="/vetpets">
+              <VetPets />
+            </Route>
+            <Route path="/notes">
+              <VetNotes />
             </Route>
             <Route path="/signup">
-              <SignUp />
+              <VetSignUp />
+            </Route>
+            <Route path="/login">
+              <OwnerLogin />
+            </Route>
+            <Route path="/notes">
+              <OwnerNotes />
+            </Route>
+            <Route path="/mypets">
+              <OwnerHome />
+            </Route>
+            <Route path="/signup">
+              <OwnerSignUp />
+            </Route>
+            <Route path="/addnote">
+              <AddNotes />
+            </Route>
+            <Route path="/addpet">
+              <AddPets />
             </Route>
             {/* Use PrivateRoute for pages that should only be accessible to a
             user that has logged in.*/}

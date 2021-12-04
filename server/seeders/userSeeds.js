7e644ba958679234c826677af8116861e9b7df5d
Ezhko <ucsd.camp.21@gmail.com>
2021-12-03 18:54:00 -0800

    add seeds

---
+++ server/seeders/userSeeds.js
@@ -0,0 +1,20 @@
+[
+    { 
+"username": "user", 
+"password": "user", 
+"pets": [
+    { "firstName": "pet", 
+"lastName": "petovski",
+"breed": "pet",
+"species": "draco",
+"notes": [
+    {"subject": "about draco",
+"text": "very healthy"
+ }, {
+     "subject": "about petovski",
+     "text": "even healthier"
+ }
+] }
+] 
+}
+];

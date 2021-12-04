9a9bc0fba162947f296accc41a269c943c179b24
Ezhko <ucsd.camp.21@gmail.com>
2021-12-04 13:09:31 -0800

    addUser and login mutations

--- client/src/util/mutations.js
+++ client/src/util/mutations.js
@@ -1,16 +1,16 @@
 import { gql } from "@apollo/client";
 
 export const CREATE_USER = gql`
-  mutation CreateUser($email: String!, $password: String!, $username: String!) {
-    createUser(email: $email, password: $password, username: $username) {
+  mutation AddUser($password: String!, $username: String!) {
+    addUser(password: $password, username: $username) {
       token
     }
   }
 `;
 
 export const LOGIN = gql`
-  mutation Login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
+  mutation Login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
       token
     }
   }
